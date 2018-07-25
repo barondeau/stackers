bba3c537ebff6bf4b72eea892ae012138e2a2351
barondeau <mbaron510129@gmail.com>
2018-07-25 14:13:30 -0500

    v0.2.0 stopping

--- stackers.py
+++ stackers.py
@@ -9,21 +9,27 @@ sense.clear()
 class stack():
 	def __init__(self):
 		pygame.init()
-		pygame.display.set_mode((640,480))
+		pygame.display.set_mode((200,480))
 		self.gaming =True
 
 	def startGame(self):
-		pygame.time.set_timer(USEREVENT +1,99900)
+		counter=0
+		pygame.time.set_timer(USEREVENT +1, 800)
 		while self.gaming:
-			counter=0
+			
 			for event in pygame.event.get():
-					while counter<=7:
-						sense.set_pixel(counter,7,(165,42,42))
-						time.sleep(0.1)
-						sense.clear()
-						counter=counter+1
-						if counter>7:
-							counter=0
+					if event.type ==KEYDOWN:
+							sense.set_pixel(counter,7,(255,0,0))
+							self.gaming = False
+					else:					
+							sense.set_pixel(counter,7,(165,42,42))
+							time.sleep(0.1)
+							sense.clear()
+							counter=counter+1
+							x=counter
+							if counter>7:
+								counter=0
+						
 
 if __name__=='__main__':
 	try:
